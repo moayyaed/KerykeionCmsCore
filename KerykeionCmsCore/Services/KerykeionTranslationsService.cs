59250623e91a77d3a09ae08da983193bf6f3edda
Jens Deweirdt <jens31@live.be>
2020-07-30 15:53:18 +0200

    Remove and sort usings

--- KerykeionCmsCore/Services/KerykeionTranslationsService.cs
+++ KerykeionCmsCore/Services/KerykeionTranslationsService.cs
@@ -1,16 +1,11 @@
-﻿using KerykeionCmsCore.Classes;
-using KerykeionCmsCore.Constants;
-using KerykeionCmsCore.Dtos;
+﻿using KerykeionCmsCore.Dtos;
 using KerykeionCmsCore.Enums;
 using KerykeionCmsCore.Options;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace KerykeionCmsCore.Services
