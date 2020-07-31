4970ae05baf1d9812253790daa3fe712f2761293
Jens Deweirdt <jens31@live.be>
2020-07-31 15:03:23 +0200

    Update ServicesExtensions.cs

--- KerykeionCms/Extensions/ServicesExtensions.cs
+++ KerykeionCms/Extensions/ServicesExtensions.cs
@@ -19,12 +19,12 @@ namespace KerykeionCms.Extensions
     public static class ServicesExtensions
     {
         /// <summary>
-        /// Adds Kerykeion Content management services using Sqlite to the application for the specified DbContext type, including a UI, also adds Identity services and UI.
+        /// Adds Kerykeion Content management services using Sqlite to the application for the specified DbContext type, including a UI, also adds Identity services.
         /// </summary>
         /// <remarks>
         /// Migrations and database still have to be made after calling this method.
         /// </remarks>
-        /// <typeparam name="TContext">The type of the DbContext the KerykeionCms needs to work with.. Has to inherit from KerykeionCmsDbContext</typeparam>
+        /// <typeparam name="TContext">The type of the DbContext the KerykeionCms needs to work with.</typeparam>
         /// <param name="services">Services to configure.</param>
         /// <param name="confOpts">Configures the KerykeionCmsOptions. Optional parameter, defaults to null</param>
         /// <returns>
@@ -37,12 +37,32 @@ namespace KerykeionCms.Extensions
         }
 
         /// <summary>
-        /// Adds Kerykeion Content management services using SqlServer to the application for the specified DbContext type, including a UI, also adds Identity services and UI.
+        /// Adds Kerykeion Content management services using Sqlite to the application for the specified DbContext type, including a UI, also adds Identity services.
         /// </summary>
         /// <remarks>
         /// Migrations and database still have to be made after calling this method.
         /// </remarks>
-        /// <typeparam name="TContext">The type of the DbContext the KerykeionCms needs to work with.. Has to inherit from KerykeionCmsDbContext</typeparam>
+        /// <typeparam name="TUser">The type of the User the KerykeionCms needs to work with.</typeparam>
+        /// <typeparam name="TContext">The type of the DbContext the KerykeionCms needs to work with.</typeparam>
+        /// <param name="services">Services to configure.</param>
+        /// <param name="confOpts">Configures the KerykeionCmsOptions. Optional parameter, defaults to null</param>
+        /// <returns>
+        /// A KerykeionCmsBuilder that can be used to further configure the KerykeionCms services.
+        /// </returns>
+        public static KerykeionCmsBuilder AddKerykeionCms<TUser, TContext>(this IServiceCollection services, Action<KerykeionCmsOptions> confOpts = null)
+            where TUser : KerykeionUser
+            where TContext : KerykeionCmsDbContext<TUser>
+        {
+            return AddBase<TUser, TContext>(services, null, confOpts);
+        }
+
+        /// <summary>
+        /// Adds Kerykeion Content management services using SqlServer to the application for the specified DbContext type, including a UI, also adds Identity services.
+        /// </summary>
+        /// <remarks>
+        /// Migrations and database still have to be made after calling this method.
+        /// </remarks>
+        /// <typeparam name="TContext">The type of the DbContext the KerykeionCms needs to work with.</typeparam>
         /// <param name="services">Services to configure.</param>
         /// <param name="connectionString">The connectionstring of the Sql server database to work with.
         /// </param>
@@ -57,13 +77,13 @@ namespace KerykeionCms.Extensions
         }
 
         /// <summary>
-        /// Adds Kerykeion Content management services using SqlServer to the application for the specified User and DbContext types, including a UI, also adds Identity services and UI.
+        /// Adds Kerykeion Content management services using SqlServer to the application for the specified User and DbContext types, including a UI, also adds Identity services.
         /// </summary>
         /// <remarks>
         /// Migrations and database still have to be made after calling this method.
         /// </remarks>
-        /// <typeparam name="TUser">The type of the User the KerykeionCms needs to work with.. Has to inherit from KerykeionUser</typeparam>
-        /// <typeparam name="TContext">The type of the DbContext the KerykeionCms needs to work with.. Has to inherit from KerykeionCmsDbContext(TUser)</typeparam>
+        /// <typeparam name="TUser">The type of the User the KerykeionCms needs to work with.</typeparam>
+        /// <typeparam name="TContext">The type of the DbContext the KerykeionCms needs to work with.</typeparam>
         /// <param name="services">Services to configure.</param>
         /// <param name="connectionString">The connectionstring of the Sql server database to work with.
         /// </param>
