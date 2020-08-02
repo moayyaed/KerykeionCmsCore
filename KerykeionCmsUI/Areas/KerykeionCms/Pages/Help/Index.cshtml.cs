e96d2fad6544039c52a3b9a96b64a47cf8937bf1
Jens Deweirdt <jens31@live.be>
2020-08-02 10:35:07 +0200

    Update Index razor page

--- KerykeionCmsUI/Areas/KerykeionCms/Pages/Help/Index.cshtml.cs
+++ KerykeionCmsUI/Areas/KerykeionCms/Pages/Help/Index.cshtml.cs
@@ -12,11 +12,17 @@ namespace KerykeionCmsUI.Areas.KerykeionCms.Pages.Help
         }
 
         public string TxtGettingStarted { get; set; }
+        public string TxtGettingStartedAddEntities { get; set; }
+        public string TxtGettingStartedUpdateEntities { get; set; }
+        public string TxtGettingStartedDeleteEntities { get; set; }
         public string TxtExtendKerykeionUser { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
             TxtGettingStarted = await TranslationsService.TranslateAsync("Getting started");
+            TxtGettingStartedAddEntities = $"{TxtGettingStarted}.. ({BtnAddValue})";
+            TxtGettingStartedUpdateEntities = $"{TxtGettingStarted}.. ({BtnUpdateValue})";
+            TxtGettingStartedDeleteEntities = $"{TxtGettingStarted}.. ({BtnDeleteValue})"; ;
             TxtExtendKerykeionUser = await TranslationsService.TranslateAsync("Extend KerykeionUser");
 
             return Page();
