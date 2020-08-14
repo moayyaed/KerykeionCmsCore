0f1ef4a15bb1129c79e599a097ad0c5bd1aed375
Jens Deweirdt <jens31@live.be>
2020-08-14 09:04:38 +0200

    Add Signal r Hub

---
+++ KerykeionCms/Hubs/KerykeionCmsHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KerykeionCms.Hubs
+{
+    public class KerykeionCmsHub : Hub
+    {
+        private readonly RoleManager<IdentityRole<Guid>> _roleManager;
+
+        public KerykeionCmsHub(RoleManager<IdentityRole<Guid>> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task SendRolesAsync()
+        {
+            await Clients.Caller.SendAsync("GetRoles", await _roleManager.Roles.OrderBy(r => r.Name).ToListAsync());
+        }
+    }
+}
