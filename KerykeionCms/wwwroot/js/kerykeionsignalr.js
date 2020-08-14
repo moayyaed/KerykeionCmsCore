bfee351afb8b138e0a9c61b2bdc756bd6250910a
Jens Deweirdt <jens31@live.be>
2020-08-14 09:04:50 +0200

    Add signal r js files

---
+++ KerykeionCms/wwwroot/js/kerykeionsignalr.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/kerykeioncmshub").build();
+
+connection.start().then(function () {
+    console.log("SignalR Connected!");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+var areRolesLoaded = false;
+connection.on("GetRoles", function () {
+    areRolesLoaded = true;
+});
+
+$(document).on("click", "#open-subnav-roles", function (event) {
+    if (!areRolesLoaded) {
+        connection.invoke("SendRolesAsync").catch(function (err) {
+            return console.error(err.toString());
+        });
+        event.preventDefault();
+    }
+});
